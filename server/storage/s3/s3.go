f49e20cb665b7a2d07a9092aa6d61c4bfdf60beb
Allen George <allen.george@datadoghq.com>
2022-06-08 18:28:47 -0400

    Implement simple in-memory-buffering S3 download

--- server/storage/s3/s3.go
+++ server/storage/s3/s3.go
@@ -1,8 +1,10 @@
 package s3
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -23,6 +25,7 @@ func New(config *Config) *Driver {
 	return &Driver{
 		client:            s3.New(config.Session),
 		uploader:          s3manager.NewUploader(config.Session),
+		downloader:        s3manager.NewDownloader(config.Session),
 		bucket:            config.Bucket,
 		checksumAlgorithm: s3.ChecksumAlgorithmSha256,
 		storageClass:      s3.StorageClassIntelligentTiering,
@@ -32,14 +35,27 @@ func New(config *Config) *Driver {
 type Driver struct {
 	client            *s3.S3
 	uploader          *s3manager.Uploader
+	downloader        *s3manager.Downloader
 	bucket            string
 	checksumAlgorithm string
 	storageClass      string
 }
 
 func (d *Driver) GetPayload(ctx context.Context, r *storage.GetRequest) (*storage.GetResponse, error) {
-	// TODO implement me
-	panic("implement me")
+	w := aws.NewWriteAtBuffer([]byte{}) // FIXME: buffers entire object in memory
+	n, err := d.downloader.DownloadWithContext(ctx, w, &s3.GetObjectInput{
+		Bucket:       &d.bucket,
+		Key:          aws.String(computeKey(r.Digest)),
+		ChecksumMode: &d.checksumAlgorithm,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &storage.GetResponse{
+		ContentLength: uint64(n),
+		Data:          io.NopCloser(bytes.NewReader(w.Bytes())),
+	}, nil
 }
 
 func (d *Driver) PutPayload(ctx context.Context, r *storage.PutRequest) (*storage.PutResponse, error) {
