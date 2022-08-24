b350b4d2ae57f68f04ed5aeeb7f1980bec560112
Hardy Ferentschik <hardy@ferentschik.de>
2022-08-24 12:29:26 +0200

    fix: Alings naming of API endpoints names

--- server/server.go
+++ server/server.go
@@ -39,7 +39,7 @@ func newV1Handler(driver storage.Driver, logger logging.Logger) http.Handler {
 		}
 		w.WriteHeader(http.StatusOK)
 	})
-	r.HandleFunc("/v1/blobs/upload", handler.putBlob)
+	r.HandleFunc("/v1/blobs/put", handler.putBlob)
 	r.HandleFunc("/v1/blobs/get", handler.getBlob)
 
 	return r
