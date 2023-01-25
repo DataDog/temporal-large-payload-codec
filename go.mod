5d6297013e2b4a1bb1bd777d28bdcefa870f675a
Eric Chee <evchee66@gmail.com>
2023-01-25 15:38:25 -0500

    azure driver

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	cloud.google.com/go/storage v1.23.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.2.0
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.6.1
 	github.com/aws/aws-sdk-go-v2 v1.16.3
 	github.com/aws/aws-sdk-go-v2/config v1.15.5
 	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.4.0
@@ -21,7 +23,10 @@ require (
 	cloud.google.com/go v0.102.1 // indirect
 	cloud.google.com/go/compute v1.7.0 // indirect
 	cloud.google.com/go/iam v0.3.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.1.4 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.0.1 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v0.7.0 // indirect
 	github.com/Microsoft/go-winio v0.5.2 // indirect
 	github.com/apache/thrift v0.0.0-20161221203622-b2a4d4ae21c7 // indirect
 	github.com/aws/aws-sdk-go v1.44.75 // indirect
@@ -71,6 +76,7 @@ require (
 	github.com/jonboulle/clockwork v0.3.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
@@ -80,6 +86,7 @@ require (
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.12.2 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
---
+++ server/storage/azure/azure.go
@@ -0,0 +1,106 @@
+package azure
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/bloberror"
+	"github.com/DataDog/temporal-large-payload-codec/server/storage"
+)
+
+// Config provides all configuration to create the Azure based driver for LPS.
+type Config struct {
+	CredOpts   *azidentity.DefaultAzureCredentialOptions
+	Container  string
+	ServiceURL string
+}
+
+type Driver struct {
+	client    *azblob.Client
+	container string
+}
+
+func New(ctx context.Context, config *Config) (*Driver, error) {
+	cred, err := azidentity.NewDefaultAzureCredential(config.CredOpts)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create azure credential: %w", err)
+	}
+	client, err := azblob.NewClient(config.ServiceURL, cred, nil)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create azure client: %w", err)
+	}
+	return &Driver{
+		client:    client,
+		container: config.Container,
+	}, nil
+}
+
+func (d *Driver) GetPayload(ctx context.Context, r *storage.GetRequest) (*storage.GetResponse, error) {
+	resp, err := d.client.DownloadStream(ctx, d.container, r.Key, nil)
+	if err != nil {
+		if bloberror.HasCode(err, bloberror.BlobNotFound) {
+			return nil, &storage.ErrBlobNotFound{Err: err}
+		}
+		return nil, err
+	}
+	numBytes, err := io.Copy(r.Writer, resp.BlobClientDownloadResponse.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return &storage.GetResponse{
+		ContentLength: uint64(numBytes),
+	}, nil
+
+}
+
+func (d *Driver) PutPayload(ctx context.Context, r *storage.PutRequest) (*storage.PutResponse, error) {
+	_, err := d.client.UploadStream(ctx, d.container, r.Key, r.Data, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &storage.PutResponse{
+		Key: r.Key,
+	}, nil
+
+}
+
+func (d *Driver) ExistPayload(ctx context.Context, r *storage.ExistRequest) (*storage.ExistResponse, error) {
+	exists := true
+	_, err := d.client.ServiceClient().NewContainerClient(d.container).NewBlobClient(r.Key).GetProperties(ctx, nil)
+	if err != nil {
+		if bloberror.HasCode(err, bloberror.BlobNotFound) {
+			exists = false
+		}
+		return nil, err
+
+	}
+	return &storage.ExistResponse{
+		Exists: exists,
+	}, nil
+
+}
+
+func (d *Driver) DeletePayload(ctx context.Context, r *storage.DeleteRequest) (*storage.DeleteResponse, error) {
+	_, err := d.client.DeleteBlob(ctx, d.container, r.Key, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &storage.DeleteResponse{}, nil
+}
+
+func (d *Driver) Validate(ctx context.Context) error {
+	_, err := d.client.ServiceClient().NewContainerClient(d.container).GetProperties(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("unable to access Azure container '%s': %s", d.container, err)
+	}
+
+	return nil
+
+}
